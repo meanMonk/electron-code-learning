604a4eb2992cedb3191e75192a3f50d80564299b
sahil kashetwar <sahilkashetwar24@gmail.com>
2022-03-28 12:45:47 +0530

    setting the position to the window.

--- boilerplates/tasky/index.js
+++ boilerplates/tasky/index.js
@@ -19,18 +19,31 @@ app.on('ready', () => {
       nodeIntegration: true,
     }
   })
-  
+
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
-  
+
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
 
-  tray = new Tray(iconPath);  
+  tray = new Tray(iconPath);
   // Implement the event handler to show and hide the browswer window.
-  tray.on('click', () => {
+  tray.on('click', (event, bounds) => {
+    // collect the position of tray icon.
+    const { x, y } = bounds;
+    // window height and width comming from here.
+    const { height, width } = mainWindow.getBounds();
+
+
     if (mainWindow.isVisible()) {
       mainWindow.hide();
     } else {
+      let yPosition = process.platform === 'darwin' ? y : y - height;
+      mainWindow.setBounds({
+        x: x - width / 2,
+        y: yPosition,
+        height,
+        width,
+      })
       mainWindow.show();
     }
   })
