c1abd0e206babcb615f170b366ffb9250165a74c
sahil kashetwar <sahilkashetwar24@gmail.com>
2022-03-28 12:39:15 +0530

    chagnes: - adding the toggle on window tray click.

--- boilerplates/tasky/index.js
+++ boilerplates/tasky/index.js
@@ -5,6 +5,7 @@ const path = require('path');
 
 
 let mainWindow;
+let tray;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
@@ -12,6 +13,7 @@ app.on('ready', () => {
     width: 300, // to set the hieght and width of browser window.
     frame: false, /// to remove the browswer border.
     resizable: false,
+    show: false, /// do not show the browswer window on launch.
     webPreferences: {
       contextIsolation: false,
       nodeIntegration: true,
@@ -23,5 +25,13 @@ app.on('ready', () => {
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
 
-  new Tray(iconPath);
+  tray = new Tray(iconPath);  
+  // Implement the event handler to show and hide the browswer window.
+  tray.on('click', () => {
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      mainWindow.show();
+    }
+  })
 })
